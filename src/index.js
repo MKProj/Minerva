4879e66bdf11e585e15e95e7fc9eeb5fbb188252
Mustafif <mustafif0929@gmail.com>
2021-09-15 10:58:21 -0400

    First Commit

---
+++ src/index.js
@@ -0,0 +1,208 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+ 
+  //Where to have custom menu
+
+const { Menu, MenuItem } = require('electron');
+
+const menu = new Menu()
+    menu.append(new MenuItem({
+        label: 'Minerva',
+        submenu: [
+        {
+            label: 'Home', 
+            accelerator: 'Ctrl+H',
+            click: () => {mainWindow.loadFile('./src/index.html')}
+        },
+         /*
+        {
+            label: '',
+            submenu: [
+              {
+                label: 'Web',
+                click:() => {mainWindow.loadFile("./src/web//index.html")}
+              },
+              {
+                label: 'PDF',
+                click: () => {mainWindow.loadFile("./src/pdf/.pdf")}
+              }
+            ]
+        },
+        {type: 'separator'},
+        */
+        {type: 'separator'},
+        {
+            label: 'Kotlin Basics',
+            submenu: [
+              {
+                label: 'Web',
+                click:() => {mainWindow.loadFile("./src/web/kt_basic/index.html")}
+              },
+              {
+                label: 'PDF',
+                click: () => {mainWindow.loadFile("./src/pdf/kotlin-basics.pdf")}
+              }
+            ]
+        },
+        {type: 'separator'},
+        {
+          label: 'C++ Basics',
+          submenu: [
+            {
+              label: 'Web',
+              click:() => {mainWindow.loadFile("./src/web/cpp_basic/index.html")}
+            },
+            {
+              label: 'PDF',
+              click: () => {mainWindow.loadFile("./src/pdf/cpp-basics.pdf")}
+            }
+          ]
+      },
+      {type: 'separator'},
+      {
+        label: 'Git Basics',
+        submenu: [
+          {
+            label: 'Web',
+            click:() => {mainWindow.loadFile("./src/web/git_basic/index.html")}
+          },
+          {
+            label: 'PDF',
+            click: () => {mainWindow.loadFile("./src/pdf/git-basics.pdf")}
+          }
+        ]
+    },
+    {type: 'separator'},
+        {
+            label: 'Quit', 
+            role: 'quit', 
+            accelerator: 'Ctrl+Q', 
+            click: function() { app.quit(); }
+        }
+        ]
+    }))
+    
+    menu.append(new MenuItem({
+     label: 'Edit',
+     submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall'
+        }
+    ]
+    }))
+    menu.append(new MenuItem({
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.reload();
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: (function() {
+            if (process.platform == 'darwin')
+              return 'Ctrl+Command+F';
+            else
+              return 'F11';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+          }
+        }
+        ]
+    }))
+    
+    menu.append(new MenuItem({
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize'
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close'
+        },
+      ]
+    }))
+  
+  Menu.setApplicationMenu(menu)
+
+    
+};
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
